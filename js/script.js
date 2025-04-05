d09b1fd9e77ec52f3e9cf4eac718f2c9a873cbd6
TrunkofCode_amit <amitchouguleack@gmail.com>
2025-04-05 05:51:39 -0400

    new changes is js

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const statusButton = document.querySelector("button");
+const pets = document.querySelector(".all-pets");
+
+const createPet = function (name, species) {
+  const pet = {
+    name: name,
+    species: species,
+    species,
+    isTired: 5,
+    sleep: function () {
+      console.log(`${this.name} needs a nap. Zzz...`);
+      this.isTired = 1;
+    },
+    play: function () {
+      if (this.isTired === 10) {
+        console.log(`${this.name} is too tired to play!`);
+      } else {
+        console.log(`Yay! ${this.name} love to play!`);
+        this.isTired += 1;
+      }
+    }
+    };
+  return pet;
+};
+
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "dog");
+const francine = createPet("Francine", "turtle");
+
+console.log(sora, clover, baxter, cleo, francine);
+
+clover.sleep();
+baxter.play();
+
+console.log(clover, baxter);
+
+// statusButton.addEventListener("click", function () {
+//   pets.innerHTML = "";
+//   pets.appendChild(sora);
+//   pets.appendChild(clover);
+//   pets.appendChild(baxter);
+//   pets.appendChild(cleo);
+//   pets.appendChild(francine);
+// });
